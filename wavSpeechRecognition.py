35109c4b3d74d32dbf12e4341f128fb7abed7656
EkiVox <k@kilian-b.fr>
2016-07-29 22:21:17 +0200

    Update wavSpeechRecognition.py

--- wavSpeechRecognition.py
+++ wavSpeechRecognition.py
@@ -29,4 +29,5 @@ for config2 in config1["configuration"]:
     phrase = config2["phrase"]
     if re.match(phrase , text):
         os.system("espeak -v french -s 150 -p 40 '" + answer + "'")
+        os.system(cmd)
 
