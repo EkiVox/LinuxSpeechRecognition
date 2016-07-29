1669011e7b2cac571e364084bac3a07f1c1cbcea
EkiVox <k@kilian-b.fr>
2016-07-29 16:29:24 +0200

    Update LinuxSpeechRecognition.py

--- LinuxSpeechRecognition.py
+++ LinuxSpeechRecognition.py
@@ -10,7 +10,7 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
 text = r.recognize_wit(audio, key="KRBQCEKL3AJQBAKO6ZXIROQ7X4EAJ76L").lower()
-print text
+print bcolors.OKBLUE "tu as dit:" + text
 with open("config.yml", 'r') as recup:
     config1 = yaml.load(recup)
 for config2 in config1["configuration"]:
@@ -18,11 +18,13 @@ for config2 in config1["configuration"]:
     cmd = config2["cmd"]
     phrase = config2["phrase"]
     if re.match(phrase , text):
-        params = {
-            'tl': 'fr',
-            'q': answer,
-        }
-        encoded = urllib.urlencode(params)
-        os.system('mpg321 \"http://translate.google.com/translate_tts?' + encoded + '\"')
-        os.system(cmd)
-	print(encoded)
+        os.system("espeak -v french -s 150 -p 40 '" + answer + "'")
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
