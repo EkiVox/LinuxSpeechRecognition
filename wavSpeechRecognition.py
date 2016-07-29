68c28fa18205c9386bb4bc212d054031addb12fe
EkiVox <k@kilian-b.fr>
2016-07-29 20:32:02 +0200

    Update wavSpeechRecognition.py

--- wavSpeechRecognition.py
+++ wavSpeechRecognition.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from os import path
 import speech_recognition as sr
 import yaml
@@ -13,13 +15,12 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-cmd = ""
-answer = ""
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio.wav")
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = r.listen(source)
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)
 text = r.recognize_wit(audio, key="KRBQCEKL3AJQBAKO6ZXIROQ7X4EAJ76L").lower()
-print bcolors.OKBLUE + bcolors.BOLD + "tu as dit: " + text
+print bcolors.OKBLUE + bcolors.BOLD + "je pense que tu as dit: " + text + "! Maintenant je vais verifier la correspondance avec le fichier de configuration"
 with open("config.yml", 'r') as recup:
     config1 = yaml.load(recup)
 for config2 in config1["configuration"]:
