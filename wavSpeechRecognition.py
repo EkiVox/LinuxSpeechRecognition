0d6961291ddc338b7e76ee8c402d1b355a72650b
EkiVox <k@kilian-b.fr>
2016-07-29 16:29:07 +0200

    Update wavSpeechRecognition.py

--- wavSpeechRecognition.py
+++ wavSpeechRecognition.py
@@ -9,7 +9,7 @@ r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)
 text = r.recognize_wit(audio, key="KRBQCEKL3AJQBAKO6ZXIROQ7X4EAJ76L").lower()
-print "tu as dit:" + text
+print bcolors.OKBLUE "tu as dit:" + text
 with open("config.yml", 'r') as recup:
     config1 = yaml.load(recup)
 for config2 in config1["configuration"]:
@@ -18,3 +18,12 @@ for config2 in config1["configuration"]:
     phrase = config2["phrase"]
     if re.match(phrase , text):
         os.system("espeak -v french -s 150 -p 40 '" + answer + "'")
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
